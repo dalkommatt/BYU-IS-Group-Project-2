c5da48e779599342f6b69e3ebc25a45e05ad09ff
Matt <matt@serolanguage.com>
2019-03-26 13:49:17 -0600

    more stuff

--- scripts.js
+++ scripts.js
@@ -1,27 +1,32 @@
-class team {
-    teamName;
-    wins;
-    losses;
-    winLossPct;
-    games;
+function playGame() {
+    class team {
+        teamName;
+        wins;
+        losses;
+        winLossPct;
+        games;
 
-    constructor(teamName) {
-        this.teamName = teamName;
+        constructor(teamName) {
+            this.teamName = teamName;
+        }
     }
-}
 
-class BasketballTeam extends team {
-
-}
-
-class Game {
-    myScore;
-    theirScore;
-    winOrLoss;
-
-    constructor(myScore, theirScore, winOrLoss) {
-        this.myScore = 
-        this.theirScore = 
-        this.winOrLoss = 
+    class BasketballTeam extends team {
+        totalPointsFor;
+        totalPointsAgainst;
     }
+
+    class Game {
+        myScore;
+        theirScore;
+        winOrLoss;
+
+        constructor(myScore, theirScore, winOrLoss) {
+            this.myScore =
+                this.theirScore =
+                this.winOrLoss =
+        }
+    }
+    var lis = document.getElementById("teamList").getElementsByTagName("li");
+    
 }
