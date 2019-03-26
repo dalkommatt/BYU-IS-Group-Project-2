a0eb77fefb29fdb2a8478079d2e2a68355938436
Matt <matt@serolanguage.com>
2019-03-26 13:18:50 -0600

    initial instructions

---
+++ scripts.js
@@ -0,0 +1,27 @@
+class team {
+    teamName;
+    wins;
+    losses;
+    winLossPct;
+    games;
+
+    constructor(teamName) {
+        this.teamName = teamName;
+    }
+}
+
+class BasketballTeam extends team {
+
+}
+
+class Game {
+    myScore;
+    theirScore;
+    winOrLoss;
+
+    constructor(myScore, theirScore, winOrLoss) {
+        this.myScore = 
+        this.theirScore = 
+        this.winOrLoss = 
+    }
+}
